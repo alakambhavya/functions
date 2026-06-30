9f81352a5e800f50ee5e49def0f8161d02d8eba1
alakambhavya <alakambhavya@gmail.com>
2026-06-30 14:33:54 +0530

    functions

---
+++ functions.py
@@ -0,0 +1,13 @@
+def sum(a,b):
+	c=a+b
+	print("sum of two numbers is :",c)
+sum(10,20)
+
+print("___________")
+
+
+def product(c,d):
+	e=c*d
+	print("product of two numbers is :",e)
+product(5,30)
+
