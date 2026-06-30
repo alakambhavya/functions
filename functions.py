65e918a64e1eb635d3050e93c4e194f09d91a604
alakambhavya <alakambhavya@gmail.com>
2026-06-30 14:35:17 +0530

    update

--- functions.py
+++ functions.py
@@ -11,3 +11,19 @@ def product(c,d):
 	print("product of two numbers is :",e)
 product(5,30)
 
+print("_____________")
+
+def div(e,f):
+	g=e/f
+	return g
+b=div(3,9)
+print(b)
+
+print("________________")
+
+def mod(b,v):
+	g=b%v
+	return g
+mod(3,9)
+print(mod(3,9))
+
